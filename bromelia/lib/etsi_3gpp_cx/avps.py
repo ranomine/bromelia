e68875406b984a34805d0c15dba7a4fc3f873ec8
Roch-Alexandre Nomine <roch@petaouchnok.org>
2025-03-19 11:47:01 +0100

    Fixed missing Server-Name

--- bromelia/lib/etsi_3gpp_cx/avps.py
+++ bromelia/lib/etsi_3gpp_cx/avps.py
@@ -16,6 +16,7 @@ from ...avps.etsi_3gpp.ts_129_229 import SupportedFeaturesAVP
 from ...avps.etsi_3gpp.ts_129_229 import VisitedNetworkIdentifierAVP
 from ...avps.etsi_3gpp.ts_129_229 import ServerAssignmentTypeAVP
 from ...avps.etsi_3gpp.ts_129_229 import ServerCapabilitiesAVP
+from ...avps.etsi_3gpp.ts_129_229 import ServerNameAVP
 
 from ...avps.ietf.rfc6733 import AuthApplicationIdAVP
 from ...avps.ietf.rfc6733 import AuthSessionStateAVP
